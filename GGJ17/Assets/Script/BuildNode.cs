38caee719cec9ccc4ea05bf00a5544c615192f7e
LEM-II\Ilja <>
2017-01-20 21:26:07 +0100

    alles toll

---
+++ GGJ17/Assets/Script/BuildNode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildNode : MonoBehaviour {
+
+    MeshRenderer renderer;
+    public Material mouseEnter, mouseExit;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<MeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseEnter()
+    {
+        renderer.material = mouseEnter;
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material = mouseExit;
+    }
+}
