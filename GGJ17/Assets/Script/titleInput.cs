9619cf235a15b9543410f2ff4471f083df490861
TheMejmun <saman14m@gmail.com>
2017-01-21 21:46:45 +0100

    TitleScreen

---
+++ GGJ17/Assets/Script/titleInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleInput : MonoBehaviour
+{
+
+    public float waitBeforeInputSec;
+    public string nextSceneName;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= waitBeforeInputSec && Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+        }
+    }
+}
