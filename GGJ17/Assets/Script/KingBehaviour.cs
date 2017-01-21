d644181004e1966aba370e9d5ebdc258cf62d2bb
AdronTech <markus.goegele@hotmail.com>
2017-01-21 23:33:12 +0100

    Army Generator

--- GGJ17/Assets/Script/KingBehaviour.cs
+++ GGJ17/Assets/Script/KingBehaviour.cs
@@ -16,10 +16,8 @@ public class KingBehaviour : MonoBehaviour {
         my = GetComponent<MyPhysics>();
         army = new List<MyPhysics>();
 
-        for (int i = 0; i < size; i++)
-        {
-            newSoldier();
-        }
+        StartCoroutine(generateArmy());
+
     }
 
     private void newSoldier()
@@ -42,18 +40,16 @@ public class KingBehaviour : MonoBehaviour {
         if (cb != null)
             cb.targets = army;
         
-        // update other list
-        foreach(MyPhysics other in army)
-        {
-            SeperationBehaviour other_sb = other.GetComponent<SeperationBehaviour>();
-            if (other_sb != null)
-                other_sb.targets.Add(solPhysics);
-
-            CohesionBehaviour other_cb = other.GetComponent<CohesionBehaviour>();
-            if (other_cb != null)
-                other_cb.targets.Add(solPhysics);
-        }
     }
 
+    IEnumerator generateArmy()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            newSoldier();
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 	
 }
