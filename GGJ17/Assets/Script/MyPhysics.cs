d3285a8efde2eba668901f72f2c5866ebcff8004
AdronTech <markus.goegele@hotmail.com>
2017-01-21 01:06:25 +0100

    Seeking

---
+++ GGJ17/Assets/Script/MyPhysics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class MyPhysics : MonoBehaviour {
+
+    public Vector3 pos;
+    public Vector3 vel;
+    public Vector3 acc;
+    public float maxVel;
+
+	void Update () {
+        pos = transform.position;
+
+        GetComponent<Rigidbody>().MovePosition(transform.position + vel * Time.deltaTime);
+
+        vel += acc * Time.deltaTime;
+
+        if (vel.magnitude > maxVel)
+        {
+            vel.Normalize();
+            vel *= maxVel;
+        }
+
+        acc = new Vector3();
+
+        foreach (Steering steering in GetComponents<Steering>())
+        {
+            SteeringOutput so = steering.getSteering();
+            acc += so.linear * steering.weight;
+        }
+
+    }
+
+}
---
+++ GGJ17/Assets/Script/Steering.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(MyPhysics))]
+public abstract class Steering : MonoBehaviour {
+
+    public float weight;
+    protected MyPhysics my;
+
+    public void Start()
+    {
+        my = GetComponent<MyPhysics>();
+    }
+
+    public abstract SteeringOutput getSteering();
+
+}
