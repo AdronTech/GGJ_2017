eec296670b220cd4ea3a5a5d8dae37b96ab3439b
doingitraith <raith.jakob@outlook.com>
2017-01-21 18:00:08 +0100

    Game Design Shenanigans
    -Tilt Shift Effect
    -UI Blueprint with new Fonts
    -ResourceManager

---
+++ GGJ17/Assets/Script/ResourceManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public float ousaResource, ousaDelta, timeWait;
+
+    // Use this for initialization
+    void Start()
+    {
+        ousaResource = 0f;
+        ousaDelta = 0f;
+        timeWait = 0f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeWait += Time.deltaTime;
+
+        if (timeWait >= 1.0f)
+        {
+            --timeWait;
+            updateResources();
+        }
+    }
+
+    private void updateResources()
+    {
+        ousaResource += ousaDelta;
+    }
+
+    public void RegisterResources(float resource_value)
+    {
+        ousaDelta += resource_value;
+    }
+
+    public void DeregisterResources(float resource_value)
+    {
+        ousaDelta -= resource_value;
+    }
+}
