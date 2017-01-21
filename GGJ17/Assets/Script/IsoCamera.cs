f2aec5f42c24932734af0163df2fb26dadaddbb3
LEM-II\Ilja <>
2017-01-21 07:18:35 +0100

    iso Cam bewegt sich. Hui schön.

---
+++ GGJ17/Assets/Script/IsoCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsoCamera : MonoBehaviour {
+
+    public float panSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 localMove = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        Vector3 groundMove = Vector3.ProjectOnPlane(transform.TransformVector(localMove), Vector3.up);
+        groundMove.Normalize();
+        transform.Translate(groundMove * panSpeed * Time.deltaTime, Space.World);
+	}
+}
